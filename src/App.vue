8a7ef1c873b5306619e0ee5511320fe83f3810f5
Frank Diaz <pachodiaz@gmail.com>
2021-10-25 19:42:58 -0500

    Continuación integracion Vue con Spring, y explicacion localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
 <img id="logo" src="@/assets/logo.png" alt="">
+<div v-if="id">
 <h1>Menú</h1>
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <a @click.prevent="cerrarSesion" href="#">Cerrar Sesión</a>
+  </div>
   </div>
   <router-view/>
 </template>
+
+<script>
+export default {
+  mounted(){
+    this.id=localStorage.cliente
+  },
+  data(){
+    return {
+      id:-1
+    }
+  },
+  methods:{
+    cerrarSesion(){
+      localStorage.clear();
+    }
+  }
+}
+</script>
+
+
 
 <style>
 #app {
